b1ac799cabd348d762d4c1665049c1efc4fe4116
Joan Marc Samó Rojas <22conan12@gmail.com>
2025-05-21 09:26:22 +0000

    Primera Pagina feta

--- web_parking/app/main.py
+++ web_parking/app/main.py
@@ -1,15 +1,44 @@
 # ~/web-robocat/main.py
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+import os
+from dotenv import load_dotenv
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi import Form
+from fastapi.responses import RedirectResponse
+import json
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+templates = Jinja2Templates(directory="app/templates")
+
+load_dotenv()
 
 @app.get("/")
 def home(request: Request):
     usuari = {"nom": "Joan Marc"}
-    return templates.TemplateResponse("index.html", {"request": request, "usuari": usuari})
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "google_maps_key": os.getenv("GOOGLE_MAPS_KEY")
+    })
 
 @app.get("/mapa")
 def parking(request: Request):
-    return templates.TemplateResponse("mapa.html", {"request": request, "usuari": "Joan Marc"})
+    return templates.TemplateResponse("mapa.html", {
+        "request": request,
+        "google_maps_key": os.getenv("GOOGLE_MAPS_KEY")
+    })
+
+@app.post("/guardar-zona")
+async def guardar_zona(nom: str = Form(...), coords: str = Form(...)):
+    zona = {
+        "nom": nom,
+        "coordenades": json.loads(coords)
+    }
+    # ⚠️ Per ara, ho guardem a fitxer (pots canviar per PostgreSQL després)
+    with open("zones.json", "a") as f:
+        f.write(json.dumps(zona) + "\n")
+
+    return RedirectResponse(url="/mapa", status_code=303)
