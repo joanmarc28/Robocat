6349d8299b6f121b300c1fb0cd282a80a047974d
NIU1679937 <22conan12@gmail.com>
2025-05-30 11:13:21 +0200

    Ultrasons

---
+++ app/sensors/ultrasonic.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO
+import time
+import config
+
+
+class ModulUltrasons:
+    """Classe per gestionar les potes del quadrúpede."""
+    def __init__(self):
+        # Configura els pins
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(config.ULTRASONIC_TRIG, GPIO.OUT)
+        GPIO.setup(config.ULTRASONIC_ECHO, GPIO.IN)
+
+    def mesura_distancia(self):
+        # Envia trigger curtet
+        GPIO.output(config.ULTRASONIC_TRIG, False)
+        time.sleep(0.0002)
+        GPIO.output(config.ULTRASONIC_TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(config.ULTRASONIC_TRIG, False)
+
+        # Espera l'eco
+        while GPIO.input(config.ULTRASONIC_ECHO) == 0:
+            start = time.time()
+        while GPIO.input(config.ULTRASONIC_ECHO) == 1:
+            end = time.time()
+
+        # Calcula temps i distancia
+        duration = end - start
+        distance = (duration * 34300) / 2  # velocitat so = 34300 cm/s
+
+        return distance
+
+try:
+    while True:
+        dist = ModulUltrasons().mesura_distancia
+        print(f"Distància: {dist:.2f} cm")
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("Sortint...")
+    GPIO.cleanup()
