b53f0fe0057fae76f7f0c5f74eec6370dff9c72e
Lara Castillejo Roig <92856231+laranyeta@users.noreply.github.com>
2025-06-21 17:10:33 +0200

    update: police_behaviour.py

--- app/modes/police_behavior.py
+++ app/modes/police_behavior.py
@@ -1,5 +1,6 @@
 import config
 import time
+from app.vision.plate_detection import PlateDetection
 
 class PoliceBehavior:
     def __init__(self, speaker, camera):
@@ -9,4 +10,17 @@ class PoliceBehavior:
     def detect_license_plate(self):
         #print("[POLICE] Detectant matrícula...")
         frame = self.camera.capture()
+        car = PlateDetection.detect_car(frame)
+        #print("[POLICE] Cotxe detectat")
+        plate = PlateDetection.detect_plate(car)
+        #print("[POLICE] Matrícula detectada")
+        if plate is not None:
+            plate_text,_,_ = PlateDetection.detect_ocr(plate)
+            #print(f"[POLICE] Matrícula reconeguda: {plate_text}")
+            return plate_text
+        else:
+            #print("[POLICE] No s'ha pogut detectar la matrícula")
+            return None
+
+
 
