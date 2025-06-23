80d2b7b66c982bd7e7d785015c5d86c7e8764533
Luis Vera Albarca <163741650+LuisVera1998@users.noreply.github.com>
2025-06-23 11:42:31 +0200

    Update esquema.sql

--- web_parking/bd/esquema.sql
+++ web_parking/bd/esquema.sql
@@ -81,3 +81,13 @@ CREATE TABLE Estada (
     Preu DECIMAL,
     Activa BOOLEAN
 );
+
+-- Taula Robot
+CREATE TABLE robot (
+    id SERIAL PRIMARY KEY,
+    nom TEXT,
+    identificador TEXT UNIQUE,
+    ip TEXT,
+    estat TEXT, -- "online", "offline"
+    ultima_connexio TIMESTAMP
+);
