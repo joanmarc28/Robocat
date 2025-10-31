b41d3a3f93fa2a88bde8d285a23abed8de0dd233
Lara Castillejo Roig <92856231+laranyeta@users.noreply.github.com>
2025-10-31 13:04:22 +0100

    Refactor detect_container method indentation

--- app/modes/city_behavior.py
+++ app/modes/city_behavior.py
@@ -17,17 +17,17 @@ class CityBehavior:
         #self.max_plates_stored = 5  # Quantes últimes guardes
         self.similarity_threshold = 0.85  # Percentatge mínim de similitud per considerar-la la mateixa
 
-        def detect_container(self, state="city", duration=3):
-            t_inici = time.time()
-            while time.time() - t_inici < duration:
-                displays_show_frames(state)
-            try:
-                frame = self.camera.capture()
-                container, _ = ContainerDetection.detect_container(frame, camera=self.camera)
-                if container is not None:
-                    print(f"[CITY] Nou container detectat")
-                    #falta que mostri el frame amb el container!
-                return None
-            except Exception as e:
-                print(f"[ERROR] Error en la detecció del container {e}")
-                return None
+    def detect_container(self, state="city", duration=3):
+        t_inici = time.time()
+        while time.time() - t_inici < duration:
+            displays_show_frames(state)
+        try:
+            frame = self.camera.capture()
+            container, _ = ContainerDetection.detect_container(frame, camera=self.camera)
+            if container is not None:
+                print(f"[CITY] Nou container detectat")
+                #falta que mostri el frame amb el container!
+            return None
+        except Exception as e:
+            print(f"[ERROR] Error en la detecció del container {e}")
+            return None
